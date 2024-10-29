182e16817fed6b2abc3136df9b2983969ed16dcd
mutiarardsl <160991558+mutiarardsl@users.noreply.github.com>
2024-10-29 23:54:28 +0700

    index.php

---
+++ apps/controller/index.php
@@ -0,0 +1,15 @@
+<?php
+
+class index{
+    public function __construct(){
+        echo "anda berada pada controller index \n";
+    }
+    
+    public function index(){
+        echo "anda memanggil function index \n";
+    }
+    
+    public function home(){
+        echo "anda memanggil action home \n";
+    } 
+}
